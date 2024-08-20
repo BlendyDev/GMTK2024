32e1b3c16c817bc7a3592e1d54c94ba0636f0080
BlendyDev <blendydev@gmail.com>
2024-08-20 10:58:56 +0200

    camera wrap fix

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,20 +1,61 @@
 extends Camera2D
 
 @export var player: CharacterBody2D
-@export var wrapSpeed: = 200.0
-@export var accel := 150.0
+@export var wrapSpeed: = 600.0
+@export var accel := 1500.0
 var currentX: float = 0
+var currentY: float = 0
+var currentScreenX :int
+var currentScreenY: int
+var widthX: float
+var widthY: float
+var endX: float
+var endY: float
+var progressX: float
+var progressY: float
+var movingX := 0
+var movingY := 0
 
 func _process(delta):
-	var cameraSize = get_viewport_rect().size / self.zoom
+	var cameraSize: Vector2 = get_viewport_rect().size / self.zoom
 	
-	if (player.global_position.x - currentX > 0.47*cameraSize.x):
-		var end = currentX-self.global_position.x + cameraSize.x * 0.95
-		var progress = (self.offset.x - currentX)/(end - currentX)
-		self.offset.x = move_toward(self.offset.x, end, delta * wrapSpeed + (pow((1 - progress), 1.5) + 0.005) * accel)
-	elif player.global_position.x - currentX < -0.47*cameraSize.x:
-		var end = currentX-self.global_position.x - cameraSize.x * 0.95
-		var progress = (self.offset.x - currentX)/(end - currentX)
-		self.offset.x = move_toward(self.offset.x, end, delta * wrapSpeed + (pow((1 - progress), 1.5) + 0.005) * accel)
-	else: currentX = (self.global_position.x + self.offset.x)
-	#global_position.x = player.global_position.x
+	widthX = cameraSize.x * 0.95
+	endX = widthX * currentScreenX
+	progressX = ((self.global_position.x + self.offset.x) - widthX * currentScreenX)/(widthX)
+	
+	widthY = cameraSize.y * 0.95
+	endY = widthY * currentScreenY
+	progressY = ((self.global_position.y + self.offset.y) - widthY * currentScreenY)/(widthY)
+	
+	if player.global_position.x - currentX > 0.47*cameraSize.x || movingX == 1:
+		movingX = 1
+		self.offset.x = move_toward(self.offset.x, endX + widthX, delta * (wrapSpeed + (pow((1 - progressX), 1.5) + 0.005) * accel))
+		if (self.offset.x == endX + widthX): updateX()
+	elif player.global_position.x - currentX < -0.47*cameraSize.x || movingX == -1:
+		movingX = -1
+		self.offset.x = move_toward(self.offset.x, endX - widthX, delta * (wrapSpeed + (pow((1 + progressX), 1.5) + 0.005) * accel))
+		if (self.offset.x == endX - widthX): updateX()
+	else:
+		updateX()
+		
+	if player.global_position.y - currentY > 0.47*cameraSize.y || movingY == 1:
+		movingY = 1
+		self.offset.y = move_toward(self.offset.y, endY + widthY, delta * (wrapSpeed + (pow((1 - progressY), 1.5) + 0.005) * accel))
+		if (self.offset.y == endY + widthY): updateY()
+	elif player.global_position.y - currentY < -0.47*cameraSize.y || movingY == -1:
+		movingY = -1
+		self.offset.y = move_toward(self.offset.y, endY - widthY, delta * (wrapSpeed + (pow((1 + progressY), 1.5) + 0.005) * accel))
+		if (self.offset.y == endY - widthY): updateY()
+	else:
+		updateY()
+	
+func updateX():
+	movingX = 0
+	currentScreenX = floor(self.global_position.x + self.offset.x / widthX)
+	currentX = (self.global_position.x + self.offset.x)
+	
+func updateY():
+	movingY = 0
+	currentScreenY = floor(self.global_position.y + self.offset.y / widthY)
+	currentY = (self.global_position.y + self.offset.y)
+	
